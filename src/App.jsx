d26b6c7abf8ecf774b66303ea6c5387c65b84db8
janidu98 <janinduchathushan@gmail.com>
2024-04-17 09:46:35 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import Consultation from "./components/Consultation/Consultation"
+import FAQs from "./components/FAQs/FAQs"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Navbar from "./components/Navbar/Navbar"
+
 function App() {
 
   return (
     <div>
-      App
+      <Navbar />
+      <Header />
+      <Consultation />
+      <FAQs />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/FAQs/FAQs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './faqs.css'
+
+const FAQs = () => {
+  return (
+    <div>
+      FAQs
+    </div>
+  )
+}
+
+export default FAQs
