0b6b1b0ab89c85543bbeef63e16199353291bb1d
janidu98 <janinduchathushan@gmail.com>
2024-04-17 10:24:58 +0530

    done navbar section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,22 @@ import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <div className="wrapper">
+        <div className="nav-container">
+            <div className="logo">
+                <img src="../../src/assets/logo.svg" alt="logo" />
+            </div>
+
+            <ul>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Careers</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
