60577bb29afa305cfae24519f30d8cf05049a94a
janidu98 <janinduchathushan@gmail.com>
2024-04-18 14:12:31 +0530

    complete responsiveness and hamburger menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 
 const Navbar = () => {
-  return (
-    <nav>
-      <div className="wrapper">
-        <div className="nav-container">
-            <div className="logo">
-                <img src="../../src/assets/logo.svg" alt="logo" />
-            </div>
 
-            <ul>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Contact Us</a></li>
-                <li><a href="#">Careers</a></li>
-            </ul>
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <>
+      <nav>
+        <div className="wrapper">
+          <div className="nav-container">
+              <div className="logo">
+                  <img src="../../src/assets/logo.svg" alt="logo" />
+              </div>
+
+              <ul>
+                  <li><a href="#">Services</a></li>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="#">Careers</a></li>
+              </ul>
+
+              <div className="menu-icon">
+                <img src="../../src/assets/menuIcon.svg" alt="menu-icon" onClick={() => setIsActive(true)}/>
+              </div>
+          </div>
         </div>
+      </nav>
+
+
+      {/* Hamburger menu */}
+      <div className={`mobile-menu-container ${isActive ? 'active' : ''}`}>
+        <div className="closeIcon">
+          <img src="../../src/assets/closeIcon.svg" alt="close-icon" onClick={() => setIsActive(false)}/>
+        </div>
+
+        <ul>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+
       </div>
-    </nav>
+      
+    </>
   )
 }
 
